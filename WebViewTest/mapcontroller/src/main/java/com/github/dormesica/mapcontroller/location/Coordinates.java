95b9fe862695ffa284e41dda5aac6347518cbf7b
Dor Mesica <dormesica10@gmail.com>
2019-04-04 09:21:06 +0300

    Add distance and azimuth methods to Coordinates.java.

--- WebViewTest/mapcontroller/src/main/java/com/github/dormesica/mapcontroller/location/Coordinates.java
+++ WebViewTest/mapcontroller/src/main/java/com/github/dormesica/mapcontroller/location/Coordinates.java
@@ -10,13 +10,38 @@ import androidx.annotation.Nullable;
  */
 public class Coordinates {
 
+    /**
+     * Calculates the distance between the 2 given points.
+     *
+     * @param from The origin location.
+     * @param to   The destination.
+     * @return The distance between from and to.
+     */
+    public static double distanceBetween(Coordinates from, Coordinates to) {
+        return from.distance(to);
+    }
+
+    /**
+     * Calculates the azimuth to the given point in degrees.
+     * <p>
+     * Azimuth is the angle between the a line from the current location to <code>to</code> and the north (a line from
+     * the current location to the north pole where the meridians meet).
+     *
+     * @param from The origin location.
+     * @param to   The destination.
+     * @return The azimuth between from and to.
+     */
+    public static double azimuthBetween(Coordinates from, Coordinates to) {
+        return from.azimuth(to);
+    }
+
     private final double lon;
     private final double lat;
     private final double alt;
 
     /**
      * Creates a new <code>Coordinates</code> instance.
-     *
+     * <p>
      * The altitude is a assigned a default value of 0.
      *
      * @param lon The longitude value.
@@ -70,6 +95,31 @@ public class Coordinates {
      */
     public double getAlt() {
         return alt;
+    }
+
+    /**
+     * Calculates the distance to the given point in meters.
+     *
+     * @param to The point to which the distance is calculated
+     * @return The distance to to
+     */
+    public double distance(Coordinates to) {
+        // TODO implement
+        return 0;
+    }
+
+    /**
+     * Calculates the azimuth to the given point in degrees.
+     * <p>
+     * Azimuth is the angle between the a line from the current location to <code>to</code> and the north (a line from
+     * the current location to the north pole where the meridians meet).
+     *
+     * @param to The location to which the azimuth is calculated.
+     * @return The azimuth to to
+     */
+    public double azimuth(Coordinates to) {
+        // TODO implement
+        return 0;
     }
 
     @Override
