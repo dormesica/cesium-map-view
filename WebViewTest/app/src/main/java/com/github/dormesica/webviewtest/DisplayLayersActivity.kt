8e38524e61e7c755208efa617573577d2c62e184
Dor Mesica <dormesica10@gmail.com>
2019-04-28 18:08:33 +0300

    Move adapter to its own file. Add click listener to list items.

--- WebViewTest/app/src/main/java/com/github/dormesica/webviewtest/DisplayLayersActivity.kt
+++ WebViewTest/app/src/main/java/com/github/dormesica/webviewtest/DisplayLayersActivity.kt
@@ -2,19 +2,16 @@ package com.github.dormesica.webviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.TextView
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.dormesica.mapcontroller.layers.VectorLayer
+import com.github.dormesica.webviewtest.adapters.VectorLayerAdapter
 
 class DisplayLayersActivity : AppCompatActivity() {
 
     private lateinit var recycleView: RecyclerView
-    private lateinit var layersAdapter: RecyclerView.Adapter<*>
+    private lateinit var layersAdapter: VectorLayerAdapter
     private lateinit var viewManager: LinearLayoutManager
 
     companion object {
@@ -30,36 +27,14 @@ class DisplayLayersActivity : AppCompatActivity() {
         val layers = intent.getParcelableArrayListExtra<VectorLayer>(EXTRA_LAYER_LIST)
 
         viewManager = LinearLayoutManager(this)
-        layersAdapter = LayersAdapter(layers)
+        layersAdapter = VectorLayerAdapter(layers)
+        layersAdapter.setOnItemClickListener {
+            Log.d(TAG, " ${it.name} layer clicked")
+        }
         recycleView = findViewById<RecyclerView>(R.id.rv_layers).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = layersAdapter
-        }
-    }
-
-    class LayersAdapter(list: List<VectorLayer>): RecyclerView.Adapter<LayersAdapter.ViewHolder>() {
-        class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-            val nameTextView: TextView = itemView.findViewById(R.id.tv_layer_name)
-            val descriptionTextView: TextView = itemView.findViewById(R.id.tv_layer_description)
-            val isShownCheckBox: CheckBox = itemView.findViewById(R.id.cb_is_layer_showing)
-        }
-
-        private val layersList: List<VectorLayer> = list
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.layer_item, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun getItemCount(): Int {
-            return layersList.size
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            holder.nameTextView.text = layersList[position].name
-            holder.descriptionTextView.text = layersList[position].description
-            holder.isShownCheckBox.isChecked = layersList[position].isVisible
         }
     }
 }
---
+++ WebViewTest/app/src/main/java/com/github/dormesica/webviewtest/adapters/VectorLayerAdapter.kt
@@ -0,0 +1,57 @@
+package com.github.dormesica.webviewtest.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.dormesica.mapcontroller.layers.VectorLayer
+import com.github.dormesica.webviewtest.R
+
+class VectorLayerAdapter(list: List<VectorLayer>): RecyclerView.Adapter<VectorLayerAdapter.ViewHolder>() {
+
+    var listener: ((VectorLayer) -> Unit)? = null
+
+    interface ListItemClickListener {
+        fun onListItemClick(clickedLayer: VectorLayer)
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.tv_layer_name)
+        private val descriptionTextView: TextView = itemView.findViewById(R.id.tv_layer_description)
+        private val isShownCheckBox: CheckBox = itemView.findViewById(R.id.cb_is_layer_showing)
+
+        init {
+            itemView.setOnClickListener {
+                listener?.invoke(layersList[adapterPosition])
+            }
+        }
+
+        fun bind(layer: VectorLayer) {
+           nameTextView.text = layer.name
+           descriptionTextView.text = layer.description
+           isShownCheckBox.isChecked = layer.isVisible
+        }
+    }
+
+    private val layersList: List<VectorLayer> = list
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layer_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return layersList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(layersList[position])
+    }
+
+    fun setOnItemClickListener(listener: (VectorLayer) -> Unit) {
+        this.listener = listener
+    }
+}
